6a5a5f05882a891ba7d9ebe8845aae868d9be37e
Matt Hems <mhems1@binghamton.edu>
2015-09-05 21:06:58 -0400

    implemented and tested query parsing

--- metroid.py
+++ metroid.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
+
 # Functionality for collectible query database for video game Metroid Prime
 
-import json
+import json, argparse, sys
+import query_parser
 
 class Database():
     """Mechanism for storing and querying items"""
@@ -8,24 +11,28 @@ class Database():
     def parse_items(self, filename):
         """Parse items in filename into database"""
         with open(filename, 'r') as f:
-            self.items = json.load(f)
+            self.__items = json.load(f)
+
+    @property
+    def items(self):
+        return self.__items['itemlist']
 
     def print_json(self, filename):
         """Print database to filename in json format"""
         with open(filename, 'w') as f:
-            json.dump(self.items, f, indent=2)
+            json.dump(self.__items, f, indent=2)
 
     def print_html(self, filename):
         """Print database to filename in html format"""
         pass
 
-    def query(self, **constraints):
+    def query(self, string):
         """Query database for all items satisfying constraints"""
-        # TODO
-        pass
+        self.func = query_parser.parse(string)
+        return [str(e) for e in self.items if self.func(e)]
 
     def __len__(self):
-        return len(d.items['itemlist'])
+        return len(d.items)
     
     def __str__(self):
         return '\n'.join(str(i) for i in self.items)
@@ -34,9 +41,30 @@ class Database():
         return '\n'.join(repr(i) for i in self.items)
     
 if __name__ == '__main__':
-    # establish command line options
-    # check arguments
-    # call appropriate function
+    # usage ./metroid.py [-c] [-q [-AAAA NNNN]+] [-o FILE] DATABASE
+    parser = argparse.ArgumentParser(description="Metriod Prime Collectible Query")
+    parser.add_argument('-o', '--output-file',
+                        dest    = 'output',
+                        default = sys.stdout,
+                        metavar = 'OUTPUT_FILE',
+                        help    = 'the location of the output file')
+    parser.add_argument('-c', '--count',
+                        action = 'store_true',
+                        default = False,
+                        help    = 'return the number of items satisfying the query')
+    parser.add_argument('-q', '--query',
+                        dest = 'query_string',
+                        metavar = 'QUERY_STRING',
+                        help    = 'the SQL-like query string to execute '
+                                  'of the form \'select items where\'')
+    parser.add_argument('database',
+                        metavar = 'DATABASE',
+                        help    = 'the database to query')
+    args = parser.parse_args()
     d = Database()
-    d.parse_items('all.json')
-    d.print_json('test.json')
+    d.parse_items(args.database)
+    results = d.query(args.query_string)
+    if args.count:
+        print(len(results))
+    else:
+        print('\n'.join(results))
---
+++ query_parser.py
@@ -0,0 +1,262 @@
+#!/usr/bin/python3
+
+# Parsing functionality for basic SQL-like query languagle of form:
+# select items where A == string1 and
+#                    B == string2 or
+#                    C != string3
+
+# Grammar:
+# START: EXPR ? ;
+# EXPR: EXPR ( AND TERM )
+#     | TERM
+#     ;
+# TERM: TERM OR FACTOR
+#     | FACTOR
+#     ;
+# FACTOR: '(' EXPR ')'
+#       | TEST
+#       ;
+# TEST: CHOICE '!'? '=' STRING ;
+# AND: '&' | 'and' ;
+# OR: '|' | 'or' ;
+# CHOICE: 'kind' | 'room' | 'world' | 'name' ;
+# STRING : [a-zA-z *]+ ;
+
+# Refactored to remove direct left recursion
+# START: EXPR ?;
+# EXPR: TERM EXPR' ;
+# EXPR': AND TERM EXPR'
+#      | empty
+#      ;
+# TERM: FACTOR TERM' ;
+# TERM': OR FACTOR TERM'
+#      | empty
+#      ;
+# FACTOR: '(' EXPR ')'
+#       | TEST
+#       ;
+# TEST: CHOICE '!'? '=' STRING ;
+# AND: '&' | 'and' ;
+# OR:  '|' | 'or'  ;
+# CHOICE : 'kind' | 'room' | 'world' | 'name' ;
+# STRING : [a-zA-z *]+ ;
+
+import sys, re
+
+# Synthesized attribute grammar that returns filter function to apply to dictionary
+
+AND_OPTIONS = ['and', '&', '&&']
+OR_OPTIONS  = ['or', '|', '||']
+SYMBOLS = ['==', '!=', '(', ')']
+choices   = {}
+stream    = None
+lookahead = None
+
+def advance():
+    global stream, lookahead
+    old = lookahead
+    lookahead = stream.pop(0)
+
+def parse(string):
+    initialize(string)
+    return parse_START()
+    
+def initialize(string):
+    global stream, choices
+    choices = {
+        'Kind': 0,
+        'Room': 0,
+        'World': 0,
+        'Name': 0
+    }
+    stream = lex(string)
+    stream.append('')
+    advance()
+
+def lex(string):
+    # TODO
+    if string is not None:
+        return string.split()
+    else:
+        return []
+    
+def parse_START():
+    if lookahead == '':
+        return lambda _ : True
+    else:
+        return parse_EXPR()
+
+def parse_EXPR():
+    t = parse_TERM()
+    e = parse_EXPR_PRIME()
+    if e is not None:
+        return merge_prime(t, e)
+    else:
+        return t
+
+def parse_EXPR_PRIME():
+    global lookahead
+    if lookahead == '':
+        return None
+    else:
+        if isAnd(lookahead):
+            a = parse_AND()
+            t = parse_TERM()
+            e = parse_EXPR_PRIME()
+            if e is not None:
+                return (a, merge_prime(t, e))
+            else:
+                return (a, t)
+    
+def parse_TERM():
+    f = parse_FACTOR()
+    t = parse_TERM_PRIME()
+    if t is not None:
+        return merge_prime(f, t)
+    else:
+        return f
+
+def merge_func(f1, op, f2):
+    if isAnd(op):
+        return lambda x : f1(x) and f2(x)
+    elif isOr(op):
+        return lambda x : f1(x) or  f2(x)
+    else:
+        error('MERGE_FUNC: unknown op ' + op)
+
+def merge_prime(function, prime):
+    if prime is not None:
+        op = prime[0]
+        f2 = prime[1]
+        return merge_func(function, op, f2)
+    else:
+        return function
+    
+def isAnd(op):
+    return op.lower() in AND_OPTIONS
+
+def isOr(op):
+    return op.lower() in OR_OPTIONS
+        
+def parse_TERM_PRIME():
+    global lookahead
+    if isAnd(lookahead):
+        return None
+    elif isOr(lookahead):
+        o = parse_OR()
+        f = parse_FACTOR()
+        t = parse_TERM_PRIME()
+        if t is not None:
+            return (o, merge_prime(f, t))
+        else:
+            return (o, f)
+    elif lookahead != '' and lookahead != ')':
+        error('TERM_PRIME: lookahead is not and or or, its ' + lookahead)
+    
+def parse_FACTOR():
+    global lookahead
+    if lookahead == '(':
+        advance()
+        r = parse_EXPR()
+        if lookahead == ')':
+            advance()
+            return r
+        else:
+            error('FACTOR: lookahead is not ), its ' + lookahead)
+    else:
+        return parse_TEST()
+    
+def parse_TEST():
+    global lookahead
+    res = parse_CHOICE()
+    if lookahead == '!=':
+        negate = True
+    elif lookahead == '==':
+        negate = False
+    else:
+        error('TEST: lookahead is not ! or =, its ' + lookahead)
+    advance()
+    string = parse_STRING()
+    f = lambda d : True if re.match(string, d[res], re.I) is not None else False
+    if negate:
+        return lambda d : not f(d)
+    else:
+        return f
+
+def parse_AND():
+    global lookahead
+    if not isAnd(lookahead):
+        error('AND: lookahead is not and, its ' + lookahead)
+    advance()
+    return 'and'
+
+def parse_OR():
+    global lookahead
+    if not isOr(lookahead):
+        error('OR: lookahead is not or, its ' + lookahead)
+    advance()
+    return 'or'
+    
+def parse_CHOICE():
+    global lookahead, choices
+    l = lookahead
+    if l not in choices:
+        error('CHOICE: lookahead is not choice, its ' + lookahead)
+    choices[l] += 1
+    if choices[l] > 1:
+        error('CHOICE: cannot have two of ' + l)
+    advance()
+    return l
+    
+def parse_STRING():
+    global lookahead
+    r = re.sub('\*', '.*', lookahead)
+    advance()
+    return r
+
+def error(msg):
+    print(msg)
+    sys.exit(1)
+
+if __name__ == '__main__':
+    tests = [
+        'Kind == foo',
+        'Room != foo',
+        'World == X and Name != bar',
+        'Name != home or Room == Y',
+        '( Room == X )',
+        '( World == X and Room != Y )' #  or kind != Z'
+    ]
+
+    # query = sys.argv[1]
+    l = [
+        {
+            'Kind':'foo',
+            'Room':'foo',
+            'World':'foo',
+            'Name':'foo'
+        },
+        {
+            'Kind':'bar',
+            'Room':'bar',
+            'World':'bar',
+            'Name':'bar'
+        },
+        {
+            'Kind':'bar',
+            'Room':'foo',
+            'World':'X',
+            'Name':'foo'
+        },
+        {
+            'Kind':'bar',
+            'Room':'X',
+            'World':'Z',
+            'Name':'home'
+        }        
+    ]
+    for query in tests:
+        initialize(query)
+        func = parse_START()
+        print('\n'.join(str(e) for e in l if func(e)))
+        print('='*40)
